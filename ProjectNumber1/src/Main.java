122b25c7e9348efe26a1c07d1b6fee7a1a25133b
ceresz <99102225+ceresz@users.noreply.github.com>
2022-03-17 16:05:03 -0700

    project 1 hand in

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     //Code your solution to problem number one here
     static int problemOne(String s){
         int count = 0;
+        //loop looks at each character
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+            //if character is a vowel, count increases by 1
             if (ch == 'a'|| ch == 'e'|| ch == 'i' || ch == 'o' || ch == 'u') {
                 count++;
             }
         }
+        //prints out total number of vowels in string s in the end
         System.out.println("Number of vowels: "+ count);
 
         return count;
@@ -17,10 +20,14 @@ public class Main {
     static int problemTwo(String s){
         String str = "bob";
         int count = 0, fromIndex = 0;
+        /*while loop finds substring and assigns index of next occurrence
+        to fromIndex to check if return value is greater than -1*/
         while ((fromIndex = s.indexOf(str, fromIndex)) != -1 ){
+            //count increases and fromIndex increases to find next occurrence of substring 'bob'
             count++;
             fromIndex++;
         }
+        //prints number of times 'bob' occurs in string s
         System.out.println("Number of times bob occurs is: " + count);
 
         return count;
@@ -31,18 +38,22 @@ public class Main {
         String str = s.substring(0, 1);
         int stringLength = s.length();
         char[] charArray = s.toCharArray();
-
+        //loop checks if characters follow order of alphabet
         for (int i = 0; i < stringLength - 1; i++) {
             if (charArray[i] <= charArray[i+1]) {
                 length++;
             }
+            //if 'i' is greater than 'i+1', they don't follow alphabetic order and length returns to 1
             else{
                 length = 1;
             }
+            //loop looks for the longest substring
             if (length > str.length()){
                 str = s.substring(i - length + 2, i + 2);
             }
         }
+        System.out.println("Longest substring in alphabetical order is: " + str);
+
         return str;
 
     }
@@ -54,6 +65,8 @@ public class Main {
         The tests will put your method through several and different Strings to test
         all possible cases.  If you have 100% success then there is no bugs in your methods.
          */
+
+        //testing if output is correct
         String s = "ababcbcd";
         int length = 1;
         String str = s.substring(0, 1);
@@ -63,12 +76,14 @@ public class Main {
         for (int i = 0; i < stringLength - 1; i++) {
             if (charArray[i] <= charArray[i+1]) {
                 length++;
+                System.out.println("when i = " + i +  " then length = " + length);
             }
             else{
                 length = 1;
             }
             if (length > str.length()){
                 str = s.substring(i - length + 2, i + 2);
+                System.out.println("str = "+str);
             }
         }
         System.out.println(str);
