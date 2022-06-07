ac06f99955073da5de65fd3c3d66e26cc497b7ec
ceresz <99102225+ceresz@users.noreply.github.com>
2022-06-07 10:21:04 -0700

    friends book hand in

---
+++ friendBook/src/main/java/com/example/friendbook/CreateFriend.java
@@ -0,0 +1,60 @@
+package com.example.friendbook;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CreateFriend {
+    private static String firstname;
+    private static Integer age;
+    private static FileReader fr;
+    private static BufferedReader br;
+    private static ArrayList<Friend> friends = new ArrayList<>();
+
+    public static ArrayList createAllFriends(String fileName) throws IOException{
+        fr = new FileReader(fileName);
+        br = new BufferedReader(fr);
+        String line;
+        String friendString = "";
+        while((line = br.readLine())!= null){
+
+            if (!line.equals(";")){
+                friendString += line;
+            }
+            else{
+                parseFriend(friendString);
+                friendString = "";
+            }
+        }
+        return friends;
+
+    }
+
+    private static void parseFriend(String string){
+        int pos = 0;
+        String firstname = "";
+        int age = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ','){
+                pos = i;
+                firstname = string.substring(0, pos);
+                age = Integer.parseInt(string.substring(pos + 1));
+            }
+        }
+        Friend newFriend = new Friend(firstname,age);
+        boolean dupe = false;
+
+        for (int i = 0; i < friends.size(); i++) {
+            if (newFriend.compareFriend(friends.get(i))){
+                dupe = true;
+            }
+        }
+        if (!dupe){
+            friends.add(newFriend);
+        }
+    }
+
+
+
+}
---
+++ friendBook/src/main/java/com/example/friendbook/Friend.java
@@ -0,0 +1,48 @@
+package com.example.friendbook;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Friend {
+    public String firstName;
+    public int age;
+
+    public Friend(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString(){
+        return "Name: " + firstName + "\tAge: " + age;
+    }
+
+    public void writeToFile(String filename) throws IOException{
+        FileWriter fw = new FileWriter(filename,true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(firstName + ",\r");
+        bw.write(age + "\r");
+        bw.write(";\r");
+        bw.close();
+    }
+
+    public boolean compareFriend(Friend p){
+        return this.firstName.equals(p.firstName);
+    }
+}
